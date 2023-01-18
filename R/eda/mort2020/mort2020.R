573b5eaf17c7f356ea9262d4de47017ad04a5282
rl627 <rl627@drexel.edu>
2023-01-18 16:28:56 -0500

    draft mort2020 eda

---
+++ R/eda/mort2020/mort2020.R
@@ -0,0 +1,26 @@
+# 0. setup ----------------------------------------------------------------
+{
+  library(tidyverse)
+  library(arrow)
+}
+
+
+# 1. Test Extract-load  -------------------------------------------------------------------------
+{
+  file = "\\\\files.drexel.edu\\colleges\\SOPH\\Shared\\UHC\\Projects\\Bilal_DP5\\dbt\\mortality\\source\\mort2020.parquet"
+  db = open_dataset(file)
+  dfa = db %>% 
+    select(age_5yr_group,
+           male,
+           hispanic,
+           race,
+           res_fips,
+           res_cbsa,
+           icd10_t1_red,
+           icd10_t2_red,
+           icd10_t3_red) %>% 
+    collect()
+  }
+
+
+
