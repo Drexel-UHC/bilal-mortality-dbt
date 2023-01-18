a06b78c0339b9c07f856d84d3ede68a8e277e289
rl627 <rl627@drexel.edu>
2023-01-18 16:29:23 -0500

    direclt load to server

--- R/extract-load/index.R
+++ R/extract-load/index.R
@@ -8,7 +8,8 @@
 # 1. Test Extract-load  -------------------------------------------------------------------------
 {
   load("raw-data/mort2020.rdata")
-  mort_dta %>% write_parquet(sink = "raw-data/mort2020.parquet")
+  mort_dta %>% 
+    write_parquet(sink = "\\\\files.drexel.edu\\colleges\\SOPH\\Shared\\UHC\\Projects\\Bilal_DP5\\dbt\\mortality\\source\\mort2020.parquet")
 }
 
 
