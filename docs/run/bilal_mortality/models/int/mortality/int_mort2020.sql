cb8afe5b47ffbd15061ef1c6d8b8599d7819cb3a
rl627 <rl627@drexel.edu>
2023-01-18 17:35:20 -0500

    rebuild

--- docs/run/bilal_mortality/models/int/mortality/int_mort2020.sql
+++
@@ -1,3 +0,0 @@
-create or replace view "csv"."int_mort2020__dbt_int" as (
-        select * from 'D:\git\bilal-mortality-dbt\external\dev/int_mort2020.csv'
-    );
--- docs/run/bilal_mortality/models/staging/mortality/stg_mort2020.sql
+++
@@ -1,3 +0,0 @@
-create or replace view "csv"."stg_mort2020__dbt_int" as (
-        select * from 'D:\git\bilal-mortality-dbt\external\dev/stg_mort2020.csv'
-    );
